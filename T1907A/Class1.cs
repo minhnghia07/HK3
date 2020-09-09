370bae17975b1f8d97b6a900843f25e394a7a526
Nguyen Minh Nghia (Aptech HN) <nghianmth1907030@fpt.edu.vn>
2020-09-09 16:37:23 +0700

    BTVN 7/9

---
+++ T1907A/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1907A
+{
+    class Class1
+    {
+        public string nameEmail { get; set; }
+        public string tieude { get; set; }
+        public string noidung { get; set; }
+
+        public Class1(string nameEmail, string tieude, string noidung)
+        {
+            this.nameEmail = nameEmail;
+            this.tieude = tieude;
+            this.noidung = noidung;
+        }
+        public override string ToString()
+        {
+            return nameEmail + "\n" + tieude  + "\n" + noidung;
+        }
+
+
+    }
+}
