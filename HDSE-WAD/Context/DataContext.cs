d046de857639beca1ccc55ec5b0428648d4639f7
Nguyen Minh Nghia (Aptech HN) <nghianmth1907030@fpt.edu.vn>
2020-10-30 16:05:50 +0700

    WAD

---
+++ HDSE-WAD/Context/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using HDSE_WAD.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HDSE_WAD.Context
+{
+    public class DataContext: DbContext 
+    {
+        public DataContext() : base("T1907AEntities")
+        {
+            
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
---
+++ HDSE-WAD/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HDSE_WAD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your department")]
+        public string Department { get; set; }
+        [Required(ErrorMessage = "Please enter your salary")]
+        public int Salary { get; set; }
+
+    }
+}
